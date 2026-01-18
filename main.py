5bfae043936c2f21bba89072685147c1515e524b
sergeysanz <sergiosanzwr@gmail.com>
2026-01-18 12:38:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -179,4 +179,6 @@ def generate_logo():
 # Main
 # ------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
